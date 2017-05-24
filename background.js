e58317190d5346269d7c429b23711ada39266bb8
reina-ibarez <reina.ibarez@speeracademy.org>
2017-05-24 14:07:23 -0500

    v0.3.0.0

--- background.js
+++ background.js
@@ -1,19 +1,4 @@
-// background.js
-
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
+chrome.browserAction.onClicked.addListener(function(activeTab){
+  var newURL = "www.google.com";
+  chrome.tabs.create({ url: newURL });
 });
-
-// This block is new!
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "open_new_tab" ) {
-      chrome.tabs.create({"url": request.url});
-    }
-  }
-);
