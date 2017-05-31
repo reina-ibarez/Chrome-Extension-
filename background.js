20312be6975415f7f4feb8d9e56a7842f336f040
reina-ibarez <reina.ibarez@speeracademy.org>
2017-05-31 13:08:39 -0500

    v0.5.0.0

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+// background.js
+
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
@@ -6,13 +8,3 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   });
 });
-
-// This block is new!
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "open_new_tab" ) {
-      chrome.tabs.create({"url": request.url});
-    }
-  }
-);
-
